1ffab00aba30f97a0cf45a82e12be3096355b975
MizanurRemon <remonmizanur@gmail.com>
2023-09-20 13:38:33 +0600

    UPLOAD

--- app/src/main/java/com/scanner/scanner/Views/Activity/LoginActivity.java
+++ app/src/main/java/com/scanner/scanner/Views/Activity/LoginActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.scanner.scanner.R;
 import com.scanner.scanner.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    String userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,42 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+
+        toggleController();
+
+
+    }
+
+    private void toggleController() {
+        if (binding.toggleGroup.getCheckedButtonId() == R.id.userButton) {
+            binding.passwordLayout.setVisibility(View.GONE);
+            userType = getApplicationContext().getResources().getString(R.string.user);
+        } else if (binding.toggleGroup.getCheckedButtonId() == R.id.adminButton) {
+            binding.passwordLayout.setVisibility(View.VISIBLE);
+            userType = getApplicationContext().getResources().getString(R.string.admin);
+        }
+        Toast.makeText(LoginActivity.this, userType, Toast.LENGTH_SHORT).show();
+        binding.toggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+
+               if(isChecked){
+                   if (group.getCheckedButtonId() == R.id.userButton) {
+                       userType = getApplicationContext().getResources().getString(R.string.user);
+                       binding.passwordLayout.setVisibility(View.GONE);
+                       Toast.makeText(LoginActivity.this, userType, Toast.LENGTH_SHORT).show();
+
+                   } else if (group.getCheckedButtonId() == R.id.adminButton) {
+                       binding.passwordLayout.setVisibility(View.VISIBLE);
+                       userType = getApplicationContext().getResources().getString(R.string.admin);
+                       Toast.makeText(LoginActivity.this, userType, Toast.LENGTH_SHORT).show();
+                   }
+               }
+
+
+
+            }
+        });
+
     }
 }
