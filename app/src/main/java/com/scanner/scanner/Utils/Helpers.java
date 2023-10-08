4bb9b682d5a3f9068a3dda6cff89301d53d0dfe5
MizanurRemon <remonmizanur@gmail.com>
2023-10-08 14:36:07 +0600

    UPLOAD

--- app/src/main/java/com/scanner/scanner/Utils/Helpers.java
+++ app/src/main/java/com/scanner/scanner/Utils/Helpers.java
@@ -1,14 +1,19 @@
 package com.scanner.scanner.Utils;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.util.Base64;
+import android.util.Base64OutputStream;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Helpers {
@@ -25,7 +30,7 @@ public class Helpers {
         return phone.matches(REGEX);
     }
 
-    public static String fileUriToBase64(Uri imageUri, ContentResolver resolver){
+    public static String fileUriToBase64(Uri imageUri, ContentResolver resolver) {
         try {
             final InputStream imageStream = resolver.openInputStream(imageUri);
             final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
@@ -34,9 +39,47 @@ public class Helpers {
             byte[] b = byteArrayOutputStream.toByteArray();
 
             return Base64.encodeToString(b, Base64.DEFAULT);
-        }catch (Exception e){
-            Log.d("dataxx", "fileUriToBase64: "+e.getMessage());
+        } catch (Exception e) {
             return "";
+        }
+    }
+
+    public static String getStringFile(File f) {
+        InputStream inputStream = null;
+        String encodedFile= "", lastVal;
+        try {
+            inputStream = new FileInputStream(f.getAbsolutePath());
+
+            byte[] buffer = new byte[10240];//specify the size to allow
+            int bytesRead;
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            Base64OutputStream output64 = new Base64OutputStream(output, Base64.DEFAULT);
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                output64.write(buffer, 0, bytesRead);
+            }
+            output64.close();
+            encodedFile =  output.toString();
+        }
+        catch (Exception e1 ) {
+            Log.d("dataxx", "getStringFile: "+ e1.toString());
+        }
+
+        lastVal = encodedFile;
+        return lastVal;
+    }
+
+    public static String convertPdfToBase64(Uri pdfUri, Context context) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(pdfUri);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            inputStream.close();
+
+            return Base64.encodeToString(bytes, Base64.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
--- app/src/main/java/com/scanner/scanner/Views/Fragments/ImageUploadFragment.java
+++ app/src/main/java/com/scanner/scanner/Views/Fragments/ImageUploadFragment.java
@@ -1,5 +1,7 @@
 package com.scanner.scanner.Views.Fragments;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 import static com.scanner.scanner.Utils.Constants.IMAGE_REQ_CODE;
 import static com.scanner.scanner.Utils.Constants.RESULT;
 
@@ -8,6 +10,7 @@ import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +24,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 
 
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,10 +39,13 @@ import com.scanner.scanner.Utils.Helpers;
 import com.scanner.scanner.Views.Activity.ImageCropperActivity;
 import com.scanner.scanner.Views.Activity.PdfViewerActivity;
 import com.scanner.scanner.databinding.FragmentImageUploadBinding;
+import com.yalantis.ucrop.util.FileUtils;
 
 import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImageItemClickListener, ImageAdapter.OnImageDeleteClickListener {
 
@@ -54,25 +61,59 @@ public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImag
     List<Uri> imageList = new ArrayList<>();
     ImageAdapter imageAdapter;
 
+    @SuppressLint("Range")
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == -1 && requestCode == IMAGE_REQ_CODE) {
-            String result = data.getStringExtra(RESULT);
-            if (result != null) {
-                filepath = Uri.parse(result);
+        if (resultCode != RESULT_CANCELED) {
+            if (resultCode == RESULT_OK && requestCode == IMAGE_REQ_CODE) {
+                String result = data.getStringExtra(RESULT);
+                if (result != null) {
+                    filepath = Uri.parse(result);
+                }
+
+                String ext = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(new File(filepath.getPath())).toString());
+                Log.d("dataxx", "onActivityResult: " + ext);
+
+                imageList.add(filepath);
+
+                setImageAdapter(imageList);
+
+                setImage(imageList.get(imageList.size() - 1));
+
+            } else if (resultCode == RESULT_OK && requestCode == 12) {
+
+
+
+                if (data != null) {
+                    Uri uri = data.getData();
+                    String uriString = uri.toString();
+                    File myFile = new File(uriString);
+                    String displayName = null;
+
+                    if (uriString.startsWith("content://")) {
+                        try (Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null)) {
+                            if (cursor != null && cursor.moveToFirst()) {
+                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            }
+                        }
+                    } else if (uriString.startsWith("file://")) {
+                        displayName = myFile.getName();
+                    }
+
+
+
+                    String str = Helpers.convertPdfToBase64(data.getData(), getActivity());
+                    Log.d("dtaxx", "onActivityResult: "+" "+str);
+
+                    Toast.makeText(getActivity(), displayName+" file picked" +str, Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(getActivity(), "file not picked", Toast.LENGTH_SHORT).show();
+                }
+
             }
-
-            String ext = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(new File(filepath.getPath())).toString());
-            Log.d("dataxx", "onActivityResult: "+ext);
-
-            imageList.add(filepath);
-
-            setImageAdapter(imageList);
-
-            setImage(imageList.get(imageList.size() - 1));
-
         }
 
     }
@@ -109,8 +150,7 @@ public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImag
             @Override
             public void onActivityResult(Uri result) {
                 if (result != null) {
-                    openPdfViewer(result);
-
+                    //  openPdfViewer(result);
                 }
             }
         });
@@ -152,8 +192,15 @@ public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImag
         return view;
     }
 
+    private void selectPdf() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), 12);
+    }
+
     private void openPdfViewer(Uri result) {
-       // Log.d("filexx", "openPdfViewer: "+Helpers.pdfToString(new File(result.getPath())));
         imageList.add(result);
         setImageAdapter(imageList);
     }
@@ -169,7 +216,7 @@ public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImag
     }
 
     private void imageSelect() {
-        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+        final CharSequence[] items = {"Camera", "Gallery", "PDF", "Cancel"};
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Add Image");
         builder.setItems(items, new DialogInterface.OnClickListener() {
@@ -192,7 +239,8 @@ public class ImageUploadFragment extends Fragment implements ImageAdapter.OnImag
                     galleryContent.launch("image/*");
 
                 } else if (items[i].equals("PDF")) {
-                    filePickContent.launch("application/*");
+                    // filePickContent.launch("application/*");
+                    selectPdf();
                 } else if (items[i].equals("Cancel")) {
                     dialog.dismiss();
                 }
